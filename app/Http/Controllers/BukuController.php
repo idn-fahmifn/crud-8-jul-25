f5cbcfaa15a7909084e5738321dd996a8b767754
idn-fahmifn <fahmifn.idn@gmail.com>
2025-07-09 13:39:52 +0700

    3.9

--- app/Http/Controllers/BukuController.php
+++ app/Http/Controllers/BukuController.php
@@ -32,61 +32,62 @@ class BukuController extends Controller
      */
     public function store(Request $request)
     {
-        $request->validate([
-            'judul' => ['required', 'string', 'min:5', 'max:50'],
-            'kategori' => ['required', 'numeric'],
-            'penerbit' => ['required', 'string', 'min:5', 'max:50'],
-            'cover' => ['required', 'file', 'max:10240', 'mimes:png,jpg,jpeg,svg,heic'],
-            'penulis' => ['required', 'string', 'min:5', 'max:50'],
-            'isbn' => ['required', 'string', 'min:3', 'max:10'],
-            'deskripsi' => ['required'],
-        ], 
-        // pesan error
-        [
-            // judul
-            'judul.required' => 'Judul wajib diisi',
-            'judul.string' => 'Judul isi dengan huruf dan angka',
-            'judul.min' => 'Judul minimal 5 karakter',
-            'judul.max' => 'Judul maksimal 50 karakter',
+        $request->validate(
+            [
+                'judul' => ['required', 'string', 'min:5', 'max:50'],
+                'kategori' => ['required', 'numeric'],
+                'penerbit' => ['required', 'string', 'min:5', 'max:50'],
+                'cover' => ['required', 'file', 'max:10240', 'mimes:png,jpg,jpeg,svg,heic'],
+                'penulis' => ['required', 'string', 'min:5', 'max:50'],
+                'isbn' => ['required', 'string', 'min:3', 'max:10'],
+                'deskripsi' => ['required'],
+            ],
+            // pesan error
+            [
+                // judul
+                'judul.required' => 'Judul wajib diisi',
+                'judul.string' => 'Judul isi dengan huruf dan angka',
+                'judul.min' => 'Judul minimal 5 karakter',
+                'judul.max' => 'Judul maksimal 50 karakter',
 
-            // kategori
-            'kategori.required' => 'Kategori wajib diisi',
-            'kategori.numeric' => 'Pilih judul pada option',
+                // kategori
+                'kategori.required' => 'Kategori wajib diisi',
+                'kategori.numeric' => 'Pilih judul pada option',
 
-            // penerbit
-            'penerbit.required' => 'Penerbit wajib diisi',
-            'penerbit.string' => 'Penerbit isi dengan huruf dan angka',
-            'penerbit.min' => 'Penerbit minimal 5 karakter',
-            'penerbit.max' => 'Penerbit maksimal 50 karakter',
+                // penerbit
+                'penerbit.required' => 'Penerbit wajib diisi',
+                'penerbit.string' => 'Penerbit isi dengan huruf dan angka',
+                'penerbit.min' => 'Penerbit minimal 5 karakter',
+                'penerbit.max' => 'Penerbit maksimal 50 karakter',
 
-            // cover
-            'cover.required' => 'Cover wajib diisi',
-            'cover.file' => 'Cover wajib berupa file',
-            'cover.mimes' => 'file yang diizinkan (png,jpg,jpeg,svg,heic)',
-            'cover.max' => 'Ukuran file hanya 10 MB',
+                // cover
+                'cover.required' => 'Cover wajib diisi',
+                'cover.file' => 'Cover wajib berupa file',
+                'cover.mimes' => 'file yang diizinkan (png,jpg,jpeg,svg,heic)',
+                'cover.max' => 'Ukuran file hanya 10 MB',
 
-            // penulis
-            'penulis.required' => 'Penulis wajib diisi',
-            'penulis.string' => 'Penulis isi dengan huruf dan angka',
-            'penulis.min' => 'Penulis minimal 5 karakter',
-            'penulis.max' => 'Penulis maksimal 50 karakter',
+                // penulis
+                'penulis.required' => 'Penulis wajib diisi',
+                'penulis.string' => 'Penulis isi dengan huruf dan angka',
+                'penulis.min' => 'Penulis minimal 5 karakter',
+                'penulis.max' => 'Penulis maksimal 50 karakter',
 
-            // isbn
-            'isbn.required' => 'ISBN wajib diisi',
-            'isbn.string' => 'ISBN isi dengan huruf dan angka',
-            'isbn.min' => 'ISBN minimal 5 karakter',
-            'isbn.max' => 'ISBN maksimal 50 karakter',
+                // isbn
+                'isbn.required' => 'ISBN wajib diisi',
+                'isbn.string' => 'ISBN isi dengan huruf dan angka',
+                'isbn.min' => 'ISBN minimal 5 karakter',
+                'isbn.max' => 'ISBN maksimal 50 karakter',
 
-            'deskripsi.required' => 'Deskripsi wajib diisi'
-        ]);
+                'deskripsi.required' => 'Deskripsi wajib diisi'
+            ]
+        );
 
         // kondisi cek apabila terdapat file yang diupload di form.
-        if($request->hasFile('cover'))
-        {
+        if ($request->hasFile('cover')) {
             $gambar = $request->file('cover'); //mengambil gambar di key cover
             $path = 'public/images/cover'; //path tempat menyimpan buku
             $format = $gambar->getClientOriginalExtension(); //mengambil format file yang diupload
-            $nama = 'cover-buku'.Carbon::now()->format('dmyhis').'.'.$format; //nama file ketika diupload
+            $nama = 'cover-buku' . Carbon::now()->format('dmyhis') . '.' . $format; //nama file ketika diupload
             $gambar->storeAs($path, $nama); //menyimpan gambar dengan path dan nama yang sudah ditentukan.
         }
 
@@ -103,7 +104,6 @@ class BukuController extends Controller
         ]);
 
         return back()->with('success', 'Buku berhasil dibuat');
-
     }
 
     /**
@@ -127,9 +127,80 @@ class BukuController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, $id)
     {
-        //
+
+        $data = Buku::find($id);
+
+        $request->validate(
+            [
+                'judul' => ['required', 'string', 'min:5', 'max:50'],
+                'kategori' => ['required', 'numeric'],
+                'penerbit' => ['required', 'string', 'min:5', 'max:50'],
+                'cover' => ['file', 'max:10240', 'mimes:png,jpg,jpeg,svg,heic'],
+                'penulis' => ['required', 'string', 'min:5', 'max:50'],
+                'isbn' => ['required', 'string', 'min:3', 'max:10'],
+                'deskripsi' => ['required'],
+            ],
+            // pesan error
+            [
+                // judul
+                'judul.required' => 'Judul wajib diisi',
+                'judul.string' => 'Judul isi dengan huruf dan angka',
+                'judul.min' => 'Judul minimal 5 karakter',
+                'judul.max' => 'Judul maksimal 50 karakter',
+
+                // kategori
+                'kategori.required' => 'Kategori wajib diisi',
+                'kategori.numeric' => 'Pilih judul pada option',
+
+                // penerbit
+                'penerbit.required' => 'Penerbit wajib diisi',
+                'penerbit.string' => 'Penerbit isi dengan huruf dan angka',
+                'penerbit.min' => 'Penerbit minimal 5 karakter',
+                'penerbit.max' => 'Penerbit maksimal 50 karakter',
+
+                // cover
+                'cover.file' => 'Cover wajib berupa file',
+                'cover.mimes' => 'file yang diizinkan (png,jpg,jpeg,svg,heic)',
+                'cover.max' => 'Ukuran file hanya 10 MB',
+
+                // penulis
+                'penulis.required' => 'Penulis wajib diisi',
+                'penulis.string' => 'Penulis isi dengan huruf dan angka',
+                'penulis.min' => 'Penulis minimal 5 karakter',
+                'penulis.max' => 'Penulis maksimal 50 karakter',
+
+                // isbn
+                'isbn.required' => 'ISBN wajib diisi',
+                'isbn.string' => 'ISBN isi dengan huruf dan angka',
+                'isbn.min' => 'ISBN minimal 5 karakter',
+                'isbn.max' => 'ISBN maksimal 50 karakter',
+
+                'deskripsi.required' => 'Deskripsi wajib diisi'
+            ]
+        );
+
+        // kondisi cek apabila terdapat file yang diupload di form.
+        if ($request->hasFile('cover')) {
+            $gambar = $request->file('cover'); //mengambil gambar di key cover
+            $path = 'public/images/cover'; //path tempat menyimpan buku
+            $format = $gambar->getClientOriginalExtension(); //mengambil format file yang diupload
+            $nama = 'cover-buku' . Carbon::now()->format('dmyhis') . '.' . $format; //nama file ketika diupload
+            $gambar->storeAs($path, $nama); //menyimpan gambar dengan path dan nama yang sudah ditentukan.
+        }
+
+        $data->update([
+            'id_kategori' => $request->kategori,
+            'judul_buku' => $request->judul,
+            'penerbit' => $request->penerbit,
+            // 'cover' => $nama,
+            'penulis' => $request->penulis,
+            'isbn' => $request->isbn,
+            'deskripsi' => $request->deskripsi
+        ]);
+
+        return back()->with('success', 'Buku berhasil dibuat');
     }
 
     /**
